99c8cf852824a138a1df062d773ec1ddeef09618
WweiweiSmile <1119062049@qq.com>
2023-03-23 00:06:36 +0800

    feature: 规划路由页面

--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -5,6 +5,12 @@ const routes = [
     path: "/",
     element: <App></App>,
   },
+  {
+    path: "videoPlay:user",
+  },
+  {
+    path: "login",
+  },
 ];
 const router = createBrowserRouter(routes);
 export default router;
