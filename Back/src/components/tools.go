31ecaa0b88d51dac76760487c27c4b13eb034e96
WweiweiSmile <1119062049@qq.com>
2023-03-24 19:14:26 +0800

    feature: 批量修改文件名脚本

---
+++ Back/src/components/tools.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var filepath string = "E:\\FFFF/"
+	files, _ := os.ReadDir(filepath)
+	for i := 0; i < len(files); i++ {
+		file := files[i]
+		err := os.Rename(filepath+file.Name(), filepath+strings.ToLower(file.Name()))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
