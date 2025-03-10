58ccaf1f0cfd516cc35a614d15a6960dd095c1e3
SlightSmiled <1119062049@qq.com>
2025-03-10 14:50:33 +0800

    style: 登录页面样式调整

--- front/src/components/Login/index.tsx
+++ front/src/components/Login/index.tsx
@@ -1,8 +1,8 @@
-import { Button, Input, Row, Col, message } from "antd";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useLogin} from '../../apis/auth';
-import { useAuthContext } from "../../hooks";
+import {Button, Input, Row, Col, message} from 'antd';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useLogin} from '../../apis/auth';
+import {useAuthContext} from '../../hooks';
 
 export type User = {
   id: number;
@@ -10,46 +10,40 @@ export type User = {
   username: string;
   password: string;
   avatar: string;
-  localPath:string
+  localPath: string
 };
 
 const Login: React.FC = () => {
-  const [username,setUsername] = useState("")
-  const [password, setPassword] = useState("");
-  const {runAsync} = useLogin()
-  const { setUser } = useAuthContext();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const {runAsync} = useLogin();
+  const {setUser} = useAuthContext();
   const navigate = useNavigate();
 
   // 登陆函数
-  const loginFn = async ( username: string,pwd: string) => {
-     const user =  await runAsync(username, pwd);
-     if(!!user){
-       setUser(user);
-       message.success("确认过眼神，你是对的人~~~~~~~");
-       setTimeout(() => {
-         navigate("/home");
-       },10);
-       return;
-     }
+  const loginFn = async (username: string, pwd: string) => {
+    const user = await runAsync(username, pwd);
+    if (!!user) {
+      setUser(user);
+      message.success('确认过眼神，你是对的人~~~~~~~');
+      setTimeout(() => {
+        navigate('/home');
+      }, 10);
+      return;
+    }
   };
 
   return (
     <div className="App">
-      <Row justify={"center"} align={"middle"}>
-        <Col>用户名：</Col>
-        <Col xs={12}>
-          <Input onChange={(e) => setUsername(e?.target?.value)}></Input>
-        </Col>
-      </Row>
-      <Row justify={"center"} align={"middle"}>
-        <Col>密码：</Col>
-        <Col xs={12}>
-          <Input.Password  onChange={(e) => setPassword(e?.target?.value)}></Input.Password>
-        </Col>
-      </Row>
-      <Row justify="center" align="middle">
-        <Button onClick={() => loginFn(username,password)}>确认</Button>
-      </Row>
+      <div className={'w-full h-screen flex items-center justify-center  '}>
+        <div className="w-80 h-80 flex flex-col gap-2">
+          <Input placeholder="请输入用户名" onChange={(e) => setUsername(e?.target?.value)}></Input>
+          <Input.Password placeholder="请输入密码" onChange={(e) => setPassword(e?.target?.value)}></Input.Password>
+          <div className="text-center">
+            <Button onClick={() => loginFn(username, password)}>确认</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
