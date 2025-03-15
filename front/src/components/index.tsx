e3a0a68133ac438788656387918c4bd969542b7d
SlightSmiled <1119062049@qq.com>
2025-03-15 23:13:33 +0800

    refactor: 删除不必要代码

--- front/src/components/index.tsx
+++ front/src/components/index.tsx
@@ -1,19 +1,7 @@
-import React, { useEffect } from "react";
-import { useNavigate, useOutlet, useLocation } from "react-router-dom";
-import { useAuthContext } from "../hooks";
-
+import React from "react";
+import { useOutlet } from "react-router-dom";
 const Index: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const outlet = useOutlet();
-  const { user } = useAuthContext();
-
-  useEffect(() => {
-    // 已登录，当前页面 === '/'，跳转到home页面
-    if (!!user && location.pathname === "/") {
-      navigate("/home");
-    }
-  }, [location.pathname,navigate,user]);
 
   return <>{outlet}</>;
 };
